50f84f0d2b5a12290bfef35190c76884d56314fe
Dayo Ojo <dayoojo732@gmail.com>
2025-10-11 19:12:10 -0400

    first script

---
+++ testfile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Simple provisioning script for DevOps Git + Vagrant demo
+
+echo "Updating packages..."
+sudo apt update -y
+
+echo "Installing Nginx..."
+sudo apt install nginx -y
+
+echo "Setting up homepage..."
+echo "<h1>Hello from Vagrant + Git + DevOps Training</h1>" | sudo tee /var/www/html/index.html
+
+echo "Restarting Nginx..."
+sudo systemctl restart nginx
+
+echo "Provisioning complete!"
