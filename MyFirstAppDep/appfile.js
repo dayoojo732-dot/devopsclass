a7195591f5f6ea9d133d9a3508277dbf9b144c65
Dayo Ojo <dayoojo732@gmail.com>
2025-11-02 16:12:22 -0500

    First Deployment

---
+++ MyFirstAppDep/appfile.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+ 
+app.get('/', (req, res) => res.send('Hello from Azure!'));
+ 
+app.listen(port, () => console.log(`Running on port ${port}`));
+ 
